565515b679e79c6d6e519cb0ee38a3df58c50dc6
Norayr Avetisyan <norayr.avetisyan.6@gmail.com>
2025-06-12 19:46:16 +0400

    add header and makefile

---
+++ Makefile
@@ -0,0 +1,8 @@
+NAME		= minishell
+
+PRINTF		= lib/ft_printf/
+LIBFT		= lib/libft/
+INCLUDES	= includes
+SRC_DIR		= src/
+
+SRC 		=
--- include/get_next_line.h
+++ include/get_next_line.h
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: noavetis <noavetis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/12 16:19:13 by noavetis          #+#    #+#             */
-/*   Updated: 2025/06/12 16:29:16 by noavetis         ###   ########.fr       */
+/*   Created: 2025/02/12 21:13:15 by noavetis          #+#    #+#             */
+/*   Updated: 2025/04/24 20:16:59 by noavetis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int	main(int argc, char **argv)
-{
-	
-}
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+size_t	check_n(const char *buffer);
+
+#endif
